39e216bb968b2bca10fe7d95e3ff4c9c3464f540
strikereport <34009803+strikereport@users.noreply.github.com>
2019-01-07 00:24:47 -0300

    Update Taller 3~1.sql

--- Taller 3~1.sql
+++ Taller 3~1.sql
@@ -25,15 +25,23 @@
 --WHERE emp_proy.codigo = proyecto.codigo and empleado.rut = proyecto.rut_jefe 
 
 --requerimiento numero 16 Listo
-SELECT proyecto.nombre , empleado.nombre
-FROM proyecto , empleado
-WHERE empleado.rut = proyecto.rut_jefe and proyecto.codigo = ( SELECT emp_proy.codigo
-                                                               FROM emp_proy
-                                                               GROUP BY codigo
-                                                               HAVING COUNT(*) > 5
-                                                             ) 
+--SELECT proyecto.nombre , empleado.nombre
+--FROM proyecto , empleado
+--WHERE empleado.rut = proyecto.rut_jefe and proyecto.codigo = ( SELECT emp_proy.codigo
+--                                                               FROM emp_proy
+--                                                               GROUP BY codigo
+--                                                               HAVING COUNT(*) > 5
+--                                                             ) 
+
+--Requerimiento 17
+
+--INSERT INTO "SYSTEM"."EMPLEADO" (RUT, NOMBRE, F_DE_CONTRATACION, F_DE_NACIMIENTO, DIRECCION) VALUES ('1222222278', 'SAIKO',TO_DATE(sysdate,'YYYY/MM/DD:HH:MI:SSAM'), TO_DATE('1997-07-03', 'YYYY-MM-DD'), 'HAITI')
 
 
+--Requerimiento 18
+
+INSERT INTO "SYSTEM"."EMPLEADO" (RUT, NOMBRE, F_DE_CONTRATACION, F_DE_NACIMIENTO, DIRECCION) VALUES ('1222222278', 'SAKO',TO_DATE(sysdate,'YYYY/MM/DD:HH:MI:SSAM'), TO_DATE('1997-07-03', 'YYYY-MM-DD'), 'colombia')
+ORA-00001: restricción única (SYSTEM.EMPLEADO_PK) violada
 
 
 
